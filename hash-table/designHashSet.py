1e50987fdf6e59967e9987341e71f5f7f87e3224
Khanh <quockhanh.abc@gmail.com>
2021-08-03 20:54:41 +0700

    make a true hash set

--- hash-table/designHashSet.py
+++ hash-table/designHashSet.py
@@ -1,30 +1,41 @@
 class MyHashSet:
 
-    def __init__(self):
+    def __init__(self, size: int = 5):
         """
         Initialize your data structure here.
         """
-        self.data = []
+        self.max_buckets = size
+        self.buckets = [None] * size
+        self.bucket_size = 4
+
+    def hash(self, value: int) -> int:
+        return value % self.max_buckets
 
     def add(self, key: int) -> None:
-        if not self.contains(key):
-            self.data.append(key)
+        bucket_index = self.hash(key)
+        bucket = self.buckets[bucket_index]
+        if bucket is not None:
+            if key not in bucket:
+                bucket.append(key)
+        else:
+            self.buckets[bucket_index] = [key]
 
     def remove(self, key: int) -> None:
-        if self.contains(key):
-            index = self.data.index(key)
-            if index > -1:
-                self.data = [*self.data[:index], *self.data[index+1:]]
+        bucket_index = self.hash(key)
+        bucket = self.buckets[bucket_index]
+        if bucket is not None and key in bucket:
+            index = bucket.index(key)
+            self.buckets[bucket_index] = [*bucket[:index], *bucket[index + 1:]]
 
     def contains(self, key: int) -> bool:
         """
         Returns true if this set contains the specified element
         """
-        for item in self.data:
-            if item == key:
-                return True
+        bucket_index = self.hash(key)
+        bucket = self.buckets[bucket_index]
+        if bucket is not None and key in bucket:
+            return True
         return False
-
 
 # Your MyHashSet object will be instantiated and called as such:
 obj = MyHashSet()
@@ -33,8 +44,8 @@ obj.add(2)
 print(obj.contains(1))
 print(obj.contains(3))
 obj.add(2)
-print(obj.data)
+print(obj.buckets)
 print(obj.contains(2))
 obj.remove(2)
-print(obj.data)
+print(obj.buckets)
 print(obj.contains(2))

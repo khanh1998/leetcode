2b111844f3c0292ea2d3cddbb20ba2775a871859
Quoc-Khanh Bui <quockhanh.abc@gmail.com>
2021-12-04 17:27:11 +0700

    patience sort

--- dp/longestIncreasingSubsequence.py
+++ dp/longestIncreasingSubsequence.py
@@ -2,7 +2,7 @@
 from typing import List
 
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS2(self, nums: List[int]) -> int:
         length = len(nums)
         l = [0] * (length + 1)
         maxLength = 1
@@ -15,9 +15,33 @@ class Solution:
                     if l[i] > maxLength:
                         maxLength = l[i]
         return maxLength
+    
+    def lengthOfLIS(self, nums):
+        tails = [0] * len(nums)
+        size = 0
+        for x in nums:
+            i, j = 0, size
+            print('-----------------------')
+            print(f'x={x} i={i} j={j}')
+            while i != j:
+                m = (i + j) // 2
+                print(f'm={m}')
+                if tails[m] < x:
+                    i = m + 1
+                    print(f'i={i}')
+                else:
+                    j = m
+                    print(f'j={j}')
+            tails[i] = x
+            print(f'tails[{i}]={x}')
+            size = max(i + 1, size)
+            print(f'size={size}')
+            print(f'tails {tails}')
+            print(f'nums {nums}')
+        return size
 
 s = Solution()
 print(s.lengthOfLIS([10,9,2,5,3,7,101,18]) == 4)
-print(s.lengthOfLIS([0,1,0,3,2,3]) == 4)
-print(s.lengthOfLIS([7,7,7,7,7,7,7]) == 1)
-print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6]) ==  6)
+#print(s.lengthOfLIS([0,1,0,3,2,3]) == 4)
+#print(s.lengthOfLIS([7,7,7,7,7,7,7]) == 1)
+#print(s.lengthOfLIS([1,3,6,7,9,4,10,5,6]) ==  6)

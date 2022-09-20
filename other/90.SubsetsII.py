f0893faf6d797d72fe2dd77fc888359b0905e9ce
Bui Quoc Khanh <bui.khanh@koinworks.com>
2022-09-20 21:20:23 +0700

    generate subsets by backtracking

--- other/90.SubsetsII.py
+++ other/90.SubsetsII.py
@@ -1,7 +1,29 @@
 from typing import List
 
 class Solution:
+    def build(self, nums: List[int], tmp: List[List[int]], i: int) -> List[List[int]]:
+        if i == len(nums):
+            return [tmp]
+        # take ith num
+        take = self.build(nums, [*tmp, nums[i]], i+1)
+
+        # don't take ith num,
+        # skips consecutive duplicated values
+        j = i + 1
+        while j < len(nums) and nums[i] == nums[j]:
+            j += 1
+
+        notTake = self.build(nums, tmp, j)
+
+        return [*take, *notTake]
+
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        #backtracking
+        nums.sort()
+        result = self.build(nums, [], 0)
+        return result
+
+    def subsetsWithDup1(self, nums: List[int]) -> List[List[int]]:
         nums.sort() #n.log(n)
         result = [[]]
         lastResLen = 0
@@ -17,4 +39,6 @@ class Solution:
             result += tmp
         return result
 s = Solution()
-print(s.subsetsWithDup(nums=[1,2,3,3,4]))
+print(s.subsetsWithDup(nums=[1,2,2]))
+print(s.subsetsWithDup(nums=[0]))
+print(s.subsetsWithDup(nums=[1,2,2,3]))

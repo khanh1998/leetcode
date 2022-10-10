2326a2b22c00cf58f3046080ae1ae549fec31ea7
Bui Quoc Khanh <bui.khanh@koinworks.com>
2022-10-10 21:19:37 +0700

    268. Missing Number

--- array/268.MissingNumber.py
+++ array/268.MissingNumber.py
@@ -3,6 +3,23 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        ''' using xor 
+            for example: nums = [1,3,2]
+            res ^= 0 ^ 1 ^ 2
+            res ^= 1 ^ 2 ^ 3
+            res ^= 3
+            res == 0
+        '''
+        n = len(nums)
+        result = 0
+        for i in range(n):
+            result ^= i # [0,n-1]
+            result ^= nums[i] # all num in array
+        result ^= n
+
+        return result
+
+    def missingNumber1(self, nums: List[int]) -> int:
         length = len(nums)
         flipZero = False # use this var to track sign of zero
         for num in nums:
